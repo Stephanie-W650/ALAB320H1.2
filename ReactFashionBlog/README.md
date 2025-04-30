fad041252510def87c15898eb9c979aa83ea7fe6
Stephanie W <liw.w235@gmail.com>
2025-04-30 00:14:35 -0400

    my third commit

--- ReactFashionBlog/README.md
+++ ReactFashionBlog/README.md
@@ -1,3 +1,4 @@
-# [Netlify Live Link](Netlify link here)
-# [GitHub Pages Live Link](GitHub Pages link of HTML version)
+# [Netlify Live Link](https://app.netlify.com/teams/stephanie-w650/sites)
+# [GitHub Pages Live Link](https://github.com/Stephanie-W650/ALAB320H1.2/tree/main)
 # [GitHub Link of HTML Version](GitHub link for HTML version)
+
--- ReactFashionBlog/src/components/Article.jsx
+++ ReactFashionBlog/src/components/Article.jsx
@@ -8,7 +8,7 @@ export default function Article() {
     
     <time datetime="2020-11-12">11/12/20</time>
     <h2>On the Street in Brooklyn</h2>
-    <img src="../images/blog-image-1.jpg" alt="image"/>
+    <img src="/images/blog-image-1.jpg" alt="image"/>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ex. Voluptatum obcaecati, consequuntur quod culpa non ipsam consectetur magnam ullam quibusdam quis deleniti sed quas rerum quam suscipit, optio doloremque.</p>
     <a href="" class="Continues">Continues ...</a>
   </article>
@@ -18,7 +18,7 @@ export default function Article() {
     
     <time datetime="2020-11-11">11/11/20</time>
     <h2>Vintage in Vogue</h2>
-    <img src="../../images/blog-image-2.jpg" alt="image2"/>
+    <img src="/images/blog-image-2.jpg" alt="image2"/>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ex. Voluptatum obcaecati, consequuntur quod culpa non ipsam consectetur magnam ullam quibusdam quis deleniti sed quas rerum quam suscipit, optio doloremque.</p>
     <a href="" class="Continues">Continues ...</a>
   </article>
